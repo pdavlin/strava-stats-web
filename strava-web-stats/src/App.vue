6a0c2f5c84aba2d7da9efd194a2fe275db84f8a6
Patrick Davlin <patrickdavlin@Patricks-iMac.localdomain>
2020-08-14 20:11:33 -0500

    downgrading some things

---
+++ strava-web-stats/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id='app'>
+    <NavBar />
+    <img alt='Vue logo' src='./assets/logo.png' />
+    <HelloWorld msg='Welcome to Your Vue.js + TypeScript App' />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+import NavBar from './components/NavBar.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+    NavBar
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang='scss'>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ strava-web-stats/src/main.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+import './assets/styles/main.scss'
+
+Vue.config.productionTip = false;
+Vue.use(Buefy);
+
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
